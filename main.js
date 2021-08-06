d997feba20680915783fbed7090c4bd744343f72
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-08-06 12:07:53 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,5 +8,46 @@ function modelReady(){
 }
 
 function gotResults(error, results){
-    console.log('Got Results');
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+
+        random_color_r = Math.floor(Math.random() * 255) + 1;
+        random_color_g = Math.floor(Math.random() * 255) + 1;
+        random_color_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - ' + (results[0].confidence * 100).toFixed(2) + " %";
+
+        document.getElementById("result_label").style.color = "rgb( " + random_color_r + "," + random_color_g + "," + random_color_b +")";
+        document.getElementById("result_confidence").style.color = "rgb( " + random_color_r + "," + random_color_g + "," + random_color_b +")";
+
+        img = document.getElementById("alien-01");
+        img1 = document.getElementById("alien-02");
+        img2 = document.getElementById("alien-03");
+        img3 = document.getElementById("alien-04");
+
+        if(results[0].label == "Clap"){
+            img.src = "aliens-01.gif";
+            img1.src = "aliens-02.png";
+            img2.src = "aliens-03.png";
+            img3.src = "aliens-04.png";
+        }else if(results[0].label == "Bell"){
+            img.src = "aliens-01.png";
+            img1.src = "aliens-02.gif";
+            img2.src = "aliens-03.png";
+            img3.src = "aliens-04.png";
+        }else if(results[0].label == "Snap"){
+            img.src = "aliens-01.png";
+            img1.src = "aliens-02.png";
+            img2.src = "aliens-03.gif";
+            img3.src = "aliens-04.png";
+        }else{
+            img.src = "aliens-01.png";
+            img1.src = "aliens-02.png";
+            img2.src = "aliens-03.png";
+            img3.src = "aliens-04.gif"; 
+        }
+    }
 }
